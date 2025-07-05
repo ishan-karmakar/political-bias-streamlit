dc80a19973626eae7765ef0e634785720af815a6
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-07-04 21:12:38 -0500

    feat: Changed the whole thing

--- reader.py
+++ reader.py
@@ -2,11 +2,33 @@ import json
 import datetime
 import streamlit as st
 import plotly.graph_objects as pgo
-from plotly import subplots
 import pandas as pd
+from collections import OrderedDict
 
 with open("responses.json") as file:
     data = json.load(file)
+
+dates = []
+left = []
+center = []
+right = []
+aggregations = OrderedDict()
+for record in data:
+    dt = datetime.datetime.fromtimestamp(
+        record["submission"]["created_utc"], datetime.UTC
+    ).date()
+    if not len(dates) or dt != dates[-1]:
+        dates.append(dt)
+        left.append(0)
+        right.append(0)
+        center.append(0)
+    match record["rating"]["label"]:
+        case "left":
+            left[-1] += 1
+        case "right":
+            right[-1] += 1
+        case "center":
+            center[-1] += 1
 
 df = pd.json_normalize(data)
 df["submission.created_utc"] = df["submission.created_utc"].apply(
@@ -16,17 +38,22 @@ df["submission.created_utc"] = df["submission.created_utc"].apply(
 st.set_page_config(layout="wide")
 st.title("Political Bias of Reddit r/news over Time")
 
-fig = subplots.make_subplots(specs=[[{"secondary_y": True}]])
-fig.add_trace(
-    pgo.Scatter(
-        x=df["submission.created_utc"],
-        y=df["rating.label"],
-        name="Ratings",
-        mode="markers",
-    ),
-    secondary_y=False,
+st.write(
+    """\
+This project uses the PRAW API to get the latest submissions from the r/news subreddit.  
+The titles of the submissions are fed into a [political bias model](https://huggingface.co/matous-volf/political-leaning-politics) and are categorized as left leaning, center, or right leaning.  
+The data is then stored into a JSON file and is read by the Streamlit app to be graphed by Plotly.
+"""
 )
-fig.update_xaxes(title_text="Time")
+
+fig = pgo.Figure(
+    data=[
+        pgo.Bar(name="Left", x=dates, y=left, marker_color="#00aef3"),
+        pgo.Bar(name="Center", x=dates, y=center, marker_color="white"),
+        pgo.Bar(name="Right", x=dates, y=right, marker_color="#E81B23"),
+    ]
+)
+fig.update_layout(barmode="stack")
 
 points = st.plotly_chart(fig, on_select="rerun")["selection"]["points"]
 points = [datetime.date.fromisoformat(p["x"]) for p in points]
